bf1e3a2c786848c0ca90eaa10eb35bf4f19e35d0
JPinteligence <138131507+JPinteligence@users.noreply.github.com>
2023-07-09 18:19:20 -0400

    Create login.php

---
+++ server/registration/login.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+   <title>user registration system DEALER</title>
+  <link rel="atylesheet" type="text/css" href="atyle.css">
+</head>
+</body>
+ <div class="header">
+   <2>iniciar sesion</h2>
+   </div>
+     <form method="post" action="login.php">
+      <div class="input-group">
+          <label>Username</label>
+          <input type="tex" name="username">
+      </div>
+       <div class="input-group">
+         <label>Password</label>
+         <input type="password" name="password_1">
+       </div>
+       <div class=input-group">
+          <botton type="submit" name="login" class="btn">login</botton> 
+       </div>
+         <p>
+             Todavia no eres miembro de los distribuidores? <a hret="registrar.php">registrate</a>
+         </p>
+     </form>/
+   </body>
+   </html>
+     
+     l>
