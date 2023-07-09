ed73b35e97d8992c2bb36cf0eac59a8c581e8418
JPinteligence <138131507+JPinteligence@users.noreply.github.com>
2023-07-09 18:33:36 -0400

    Create registrar.php

---
+++ server/registration/registrar.php
@@ -0,0 +1,14 @@
+<?php include(registrar.php");?>
+<!DOCTYPE html>
+<html>
+    <title>user registration system DEALER</title>
+    <link rel="atyesheet" type=text/css" href="style.css">
+</head>
+  <body>
+    <div cless="header">
+       <h2>Registrar</h2>
+    </div>
+     <form method="post" action="registrar.php">
+       <div class=
+  </body>
+</html>
