15e3fe9bbf945cec7b6cc84b2cb09645b33aacdb
JPinteligence <138131507+JPinteligence@users.noreply.github.com>
2023-07-09 19:47:28 -0400

    Update server.php

--- server/registration/server.php
+++ server/registration/server.php
@@ -8,4 +8,11 @@ $db = mysql_connect ("localhost" , "root" , "", "registration" );
 
 // if the registrar buttonn is clicked 
 if (isset ($_post["register"])) {
-  $username
+  $username = mysql_real_escape_string($_post("username"]); 
+  $email = mysql_real_escape_string($_post("email"]);
+  $password_1 = mysql_real_escape_string($_post(Password_1"]);
+  $password_2 = mysql_real_escape_string($_post(password_2"]);
+  
+
+  
+  
