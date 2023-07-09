cbaeeeb50a1660d57af26415d2f80b8faad59fce
JPinteligence <138131507+JPinteligence@users.noreply.github.com>
2023-07-09 19:24:30 -0400

    Update registrar.php

--- server/registration/registrar.php
+++ server/registration/registrar.php
@@ -9,6 +9,29 @@
        <h2>Registrar</h2>
     </div>
      <form method="post" action="registrar.php">
-       <div class=
+       <div class="input-group">
+           <label>username</label>
+           <input type="text" name="username">
+           </div>
+    <div class="input-groud">
+        <label>Email</label>
+        <input type="text" name="email">
+    </div>
+   <div class="input-group">
+    <label>Password</label>
+       <input type type="password" name="Password_1">
+   </div>
+  <div class="input-gronp">
+      <label>confirmar Passsword</label>
+  <input type="Password" name="passwod_2">
+  </div>
+    <div class=·input-group">
+         <button type="submit" name="registrar" class="btn">registrar</button>
+    </div>
+         <p>
+             Ya eres usuario App distribuidores? <a href="login.php">iniciar Sesion</a> 
+         </p>
+         
+     </form>
   </body>
 </html>
