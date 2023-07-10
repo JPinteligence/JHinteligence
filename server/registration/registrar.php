224a9ba1c22f650b3dac3a8e124d2387085ea9ff
JPinteligence <138131507+JPinteligence@users.noreply.github.com>
2023-07-09 21:46:26 -0400

    Update registrar.php

--- server/registration/registrar.php
+++ server/registration/registrar.php
@@ -1,37 +1,38 @@
 <!DOCTYPE html>
 <html>
-    <head>
-    <title>user registration system DEALER</title>
-    <link rel="atyesheet" type=text/css" href="style.css">
+<head>
+<title>User registration system DEALER </title>
+<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-  <body>
-    <div cless="header">
-       <h2>Registrar</h2>
+<body>
+    <div class="header">
+         <h2>Registrar</h2>  
     </div>
-     <form method="post" action="registrar.php">
-       <div class="input-group">
-           <label>username</label>
-           <input type="text" name="username">
-           </div>
-    <div class="input-groud">
+
+    <form method="post" action="registrar.php">
+<div class="input-group">
+     <label>Username</label>
+     <input type="text" name="Username">
+    </div>
+    <div class="input-group">
         <label>Email</label>
-        <input type="text" name="email">
+        <input type="text" name="Email">
     </div>
-   <div class="input-group">
-       <label>Password</label>
-       <input type= "password" name="password_1">
-   </div>
-  <div class="input-gronp">
-      <label>confirmar Passsword</label>
-      <input type="password" name="passwod_2">
-  </div>
-    <div class=·input-group">
-         <button type="submit" name="registrar" class="btn">registrar</button>
+    <div class="input-group">
+        <label>Password</label>
+        <input type="password" name="password_1">
     </div>
-         <p>
-             Ya eres usuario App distribuidores? <a href="login.php">iniciar Sesion</a> 
-         </p>
-         
-     </form>
-  </body>
+       <div class="input-group">
+    <label>confirmar Password</label>
+    <input type="password" name="password_2">
+    </div>
+    <div class="input-group">
+    <button type="submit" name="https://jpjovic.app/server/registration/registrar.php" class="btn">Registrar</button>
+       </div>
+       <p>
+        Ya eres usuario? <a href="login.php">Iniciar sesión</a>
+       </p>
+    </form>
+
+</body>
 </html>
