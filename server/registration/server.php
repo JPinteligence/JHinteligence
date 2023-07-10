2f6d59e90e47bb9abbede14eac548d97b6bb6f26
JPinteligence <138131507+JPinteligence@users.noreply.github.com>
2023-07-09 20:11:12 -0400

    Update server.php

--- server/registration/server.php
+++ server/registration/server.php
@@ -10,8 +10,25 @@ $db = mysql_connect ("localhost" , "root" , "", "registration" );
 if (isset ($_post["register"])) {
   $username = mysql_real_escape_string($_post("username"]); 
   $email = mysql_real_escape_string($_post("email"]);
-  $password_1 = mysql_real_escape_string($_post(password_1"]);
-  $password_2 = mysql_real_escape_string($_post(password_2"]);
+  $password_1 = mysql_real_escape_string($_post("password_1"]);
+  $password_2 = mysql_real_escape_string($_post("password_2"]);
+
+  //ensure the form fields are filied properly
+  if (empty($usurname)) {
+     array_push($errors, "username is requiered");
+}
+if(empty(&email)) {
+        array_push ($errors, "email is required");
+}
+if (empty($pasword_1)) {
+   array_push($errors, "pasword is required");
+}
+if ($password_1 != $pasword_2) {
+   array_push($errors, "the two password do not match");
+}  
+ // if there a no errors, save user to database
+  if(count($errors) == 0=) {
+     $
   
 
   
