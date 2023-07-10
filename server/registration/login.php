03e77fcb85b5b9e4f6002a767cc9a9cd9a77938f
JPinteligence <138131507+JPinteligence@users.noreply.github.com>
2023-07-09 21:23:06 -0400

    Update login.php

--- server/registration/login.php
+++ server/registration/login.php
@@ -1,29 +1,29 @@
 <html>
 <head>
-   <title>user registration system DEALER</title>
-  <link rel="atylesheet" type="text/css" href="atyle.css">
+<title>User registration system using PHP and MySQL</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="header">
+         <h2>Iniciar sesión</h2>  
+    </div>
 
-
- <div class="header">
-   <h2>iniciar sesion</h2>
-   
-     <form method="post" action="login.php">
-      <div class="input-group">
-          <label>Username</label>
-          <input type="tex" name="username">
-      
-       <div class="input-group">
-         <label>Password</label>
-         <input type="password" name="password_1">
-     
-       <div class=input-group">
-          <botton type="submit" name="login" class="btn">login</botton> 
+    <form method="post" action="login.php">
+    <div class="input-group">
+     <label>Username</label>
+     <input type="text" name="Username">
+    </div>
+    <div class="input-group">
+        <label>Password</label>
+        <input type="password" name="password_1">
+    </div>
+    <div class="input-group">
+    <button type="submit" name="login" class="btn">login</button>
        </div>
-         <p>
-             Todavia no eres miembro de los distribuidores? <a hret="registrar.php">registrate</a>
-         </p>
-     </form>/
-   </body>
-   </html>
-     
+       <p>
+        Todavía no eres miembro? <a href="registrar.php">Regístrate</a>
+       </p>
+    </form>
 
+</body>
+</html>
