9e29948a4c8c2474963a9f813a29c91ccb9ec0e9
JPinteligence <138131507+JPinteligence@users.noreply.github.com>
2023-07-09 20:43:09 -0400

    Update server.php

--- server/registration/server.php
+++ server/registration/server.php
@@ -15,20 +15,30 @@ if (isset ($_post["register"])) {
 
   //ensure the form fields are filied properly
   if (empty($usurname)) {
-     array_push($errors, "username is requiered");
+     array_push($errors, "username is requiered"); // add error to errors
 }
 if(empty(&email)) {
         array_push ($errors, "email is required");
 }
 if (empty($pasword_1)) {
-   array_push($errors, "pasword is required");
+   array_push($errors, "password is required");
 }
-if ($password_1 != $pasword_2) {
+if ($password_1 != $password_2) {
    array_push($errors, "the two password do not match");
-}  
- // if there a no errors, save user to database
+} 
+// if there a no errors, save user to database
   if(count($errors) == 0=) {
-     $
+   $password = ,md5($password_1); // encrypt password nefore storing in database (segurity)
+    $sql = "INSERT INTO users (username, email, password)
+             VALUES ("username", "$email, "$password")·;
+    mysqli_query($db, $sql);
+    $_session("username") = $username;
+    $_session("success"] = "you are now logged in";
+    header("location: index.php"); // redirect to home page
+  }
+}
+?>
+  
   
 
   
